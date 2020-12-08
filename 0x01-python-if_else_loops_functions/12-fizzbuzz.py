3c357f38a65fdcb90bdb50af03476f2a0c0cd072
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 19:05:52 +0000

    Add 12-fizzbuzz.py without elif for 99

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,7 +7,5 @@ def fizzbuzz():
             print("Buzz", end=" ")
         elif n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
-        elif n == 99:  # to get new line at end
-            print(n)
         else:
-            print(n, end=" ")  # for numbers not divisible by 3 or 5
+            print("{}".format(n), end=" ")  # for numbers not divisible by 3 or 5
