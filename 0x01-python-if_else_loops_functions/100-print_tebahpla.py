20b20f986e29d64c7e59dc241d0dbd4d0400513e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 19:28:00 +0000

    Add 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in reversed(range(ord("a"), ord("z") + 1)):
-    if (i % 2) != 0:
-        i = i - 32
-    print("{:c}".format(i), end="")
+for i in reversed(range(ord("a"), ord("z") + 1)):  # +1 for range to include z
+    if (i % 2) != 0:  # if it's odd
+        i = i - 32  # make it capital
+    print("{:c}".format(i), end="")  # :c to print as char
