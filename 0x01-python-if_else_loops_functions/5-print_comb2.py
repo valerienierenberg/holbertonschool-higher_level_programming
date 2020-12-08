c6b9da332f7e32cfc2bca64937c17ffecacd9d18
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 18:41:49 +0000

    Remove comments

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 100):  # for numbers 0 to 99
-    if (n != 99):  # for numbers 0 to 98 only
-        print("{:02d}, ".format(n), end="")  # print total 2 digits each time
+for n in range(0, 100):
+    if (n != 99):
+        print("{:02d}, ".format(n), end="")
     else:
-        print("{:02d}".format(n))  # if == 99, print 99 with newline (no comma)
+        print("{:02d}".format(n))
