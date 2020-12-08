1e14e96b5fb52df96bd4da67e788745cc1aee59e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 19:00:58 +0000

    Add 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str):  # asokdgjal;
-    for i in range(len(str)):  # end of range is strlen
-        letter = ord(str[i])  # convert char to int to compare w/ ASCII values
-    if letter >= 97 and letter <= 122:  # if lowercase
-        letter = letter - 32  # convert to uppercase
-        print("{}".format(chr(letter)), end="")  # convert back to char, print
+def uppercase(str):
+    for i in range(len(str)):
+        letter = ord(str[i])
+    if letter >= 97 and letter <= 122:
+        letter = letter - 32
+        print("{}".format(chr(letter)), end="")
     print()
