2e9d46ab7c5894e0e72fd1a1938702cfbd5eb53b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 16:41:57 +0000

    Add comments to 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = abs(number)
-    print((number % 10), end="")
-    return (number % 10)
+    number = abs(number)  # abs value needed for last digit of neg #s
+    print((number % 10), end="")  # print last digit with no new line
+    return (number % 10)  # return last digit
