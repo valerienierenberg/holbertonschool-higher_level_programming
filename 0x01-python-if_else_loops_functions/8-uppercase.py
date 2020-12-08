e804156138949f9e2c4a4f9565788bca46fbf351
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 18:55:09 +0000

    Add comments back in

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        letter = ord(str[i])
-        if letter >= 97 and letter <= 122:
-            letter = letter - 32
-        print("{}".format(chr(letter)), end="")
+def uppercase(str):  # asokdgjal;
+    for i in range(len(str)):  # alsdkfjalsdfj
+        letter = ord(str[i])  # lsadkgjadls
+        if letter >= 97 and letter <= 122:  # alsd;kgjals;
+            letter = letter - 32  # lasdkfjas;
+        print("{}".format(chr(letter)), end="")  # lasdkfjasdkl
     print()
