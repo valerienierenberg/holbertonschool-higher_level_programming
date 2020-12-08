ffe2edd409a36caa6c26d4ed6592ddcce154627e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 16:14:17 +0000

    Add 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for a in range(0, 8):  # set first digit range 0-7
     for b in range(a + 1, 10):  # set 2nd digit range 1-9, b is one ahead of a
         print("{:d}{:d}".format(a, b), end=", ")  # print the two digits
-print("{:d}{:d}".format(a + 1, b))  # 89 with newline
+print("{:d}{:d}".format(a + 1, b))  # print 89 with newline
