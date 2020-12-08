4ccfed0baafdd0624703742fe314b0a5c1f5828b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 18:36:00 +0000

    Add 4-print_hexa.py without using hex function

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for n in range(0, 99):  # for numbers 0-98
-    print("{} = {}".format(n, hex(n)))  # print "num = hexadecimal of num"
+    print("{:d} = 0x{:x}".format(n, n))  # print "num = hexadecimal of num"
