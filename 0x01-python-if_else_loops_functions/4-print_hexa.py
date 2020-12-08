f8157dbec2158fecc526321b2f2257f432e55b4d
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 16:22:33 +0000

    Add comments to 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for n in range(0, 99):
-    print("{} = {}".format(n, hex(n)))
+for n in range(0, 99):  # for numbers 0-98
+    print("{} = {}".format(n, hex(n)))  # print "num = hexadecimal of num"
