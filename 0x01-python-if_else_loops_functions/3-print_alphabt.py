f0b791dcc0aa4f7509e43e2edf6009ffbfd5bb0b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-08 18:32:01 +0000

    Add 3-print_alphabt.py with for loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("abcdfghijklmnoprstuvwxyz", end="")
+for i in range(97, 123):  # in range of lowercase letters
+    if i != 101 and i != 113:  # if letter is not 'q' or 'e'
+        print("{}".format(chr(i)), end="")  # print as character
