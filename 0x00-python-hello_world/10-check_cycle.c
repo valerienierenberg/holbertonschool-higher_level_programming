c698d22a737cdf0598fdadaa6f5390e93cc16135
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-09 17:24:34 +0000

    Add 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* allowed functions are write, printf, putchar, puts, malloc, free
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+listint_t *slow = list, *fast = list;
+
+if (!list)
+return (0);
+
+while (slow && fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+
+if (slow == fast)
+return (1);
+}
+
+return (0);
+
+}
