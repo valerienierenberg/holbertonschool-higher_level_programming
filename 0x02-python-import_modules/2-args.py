8d918f822fd54acbbb37a16e17f17a2f6dbbd2ec
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-10 15:13:02 +0000

    Add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("{} arguments.".format(argc))
+
+    else:
+        print("{} arguments:".format(argc))
+
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
