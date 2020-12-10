4f188f0a8ee24fd4793446d6f493c17ac776867c
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-10 17:54:57 +0000

    Add 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,9 +2,9 @@
 if __name__ == '__main__':
     from magic_calculation_102 import add, sub
     if a < b
-        c = add(a, b)
+    c = add(a, b)
     for i in range(4, 6)
-        c = add(c, i)
+    c = add(c, i)
     return c
 else:
     return sub(a, b)
