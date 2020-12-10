1699e9cf25fabcbd5a72f2bf2ff52d10c3add61c
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-10 18:38:38 +0000

    Change . to : for 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
 
     if argc == 0:
         print("{} arguments.".format(argc))
-    
+
     elif argc == 1:
-        print("{} argument.".format(argc))
+        print("{} argument:".format(argc))
 
     else:
         print("{} arguments:".format(argc))
