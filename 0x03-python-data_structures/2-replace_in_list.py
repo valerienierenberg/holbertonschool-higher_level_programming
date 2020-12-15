181aaf7d111dd13d67cb6224db4af43ca281d23b
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 18:59:59 +0000

    Move empty string check outside of for loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,10 @@
 
 def replace_in_list(my_list, idx, element):
 
+    if not my_list:
+        return
+
     for x in range(len(my_list)):
-        if not my_list:
-            return
         if idx < 0 or idx > len(my_list):
             return (my_list)
         elif x == idx:
