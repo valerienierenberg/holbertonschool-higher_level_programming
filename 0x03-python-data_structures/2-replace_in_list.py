205056fe805ec8f90f61a1955c05a0bbd30204a8
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 19:06:22 +0000

    Change x to idx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,13 +5,10 @@ def replace_in_list(my_list, idx, element):
 
     if not my_list:
         return
-    
-    if not element:
-        return
 
     for x in range(len(my_list)):
         if idx < 0 or idx > len(my_list):
             return (my_list)
         elif x == idx:
-            my_list[x] = element
+            my_list[idx] = element
             return (my_list)
