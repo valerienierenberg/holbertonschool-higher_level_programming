046db746f3a50cc5a7e7a89155763c0c15bc1aed
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 14:23:04 +0000

    Add 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+
+    for x in range(len(my_list)):
+        if idx < 0 or idx > len(my_list):
+            return (my_list)
+        elif x == idx:
+            my_list[x] = element
+            return (my_list)
