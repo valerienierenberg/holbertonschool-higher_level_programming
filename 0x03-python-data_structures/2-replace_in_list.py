1627a0a2b51287e9e029ab39249bd7ffb0777ec6
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 18:54:13 +0000

    Add null check

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,8 @@
 def replace_in_list(my_list, idx, element):
 
     for x in range(len(my_list)):
+        if my_list == None:
+            return
         if idx < 0 or idx > len(my_list):
             return (my_list)
         elif x == idx:
