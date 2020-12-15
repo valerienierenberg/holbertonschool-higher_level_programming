211a81ac00a2ec80e1a98a5a1d43939886c5a490
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 19:11:06 +0000

    Move for loop into else

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return (my_list)
     else:
-        my_list[idx] = element
+        for x in range(len(my_list)):
+            if x == idx:
+                my_list[x] = element
         return (my_list)
