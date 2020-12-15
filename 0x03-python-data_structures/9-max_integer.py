60e3322f9e15517d67c0c346b27c600f78bdf912
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 19:29:39 +0000

    move max initialization to inside for loop

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,8 +6,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return
     else:
-        max = my_list[1]
         for x in my_list:
+            max = my_list[1]
             if x > max:
                 max = x
         return (max)
