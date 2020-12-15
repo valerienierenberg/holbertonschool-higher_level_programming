eddfed35ffc7ae8e0fc9815cb14db3e740bbe5c1
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 18:55:03 +0000

    Add check for empty list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@
 def replace_in_list(my_list, idx, element):
 
     for x in range(len(my_list)):
-        if my_list == None:
+        if my_list == "":
             return
         if idx < 0 or idx > len(my_list):
             return (my_list)
