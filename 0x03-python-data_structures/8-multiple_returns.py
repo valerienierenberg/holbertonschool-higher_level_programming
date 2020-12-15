ce4eff46424b2dbb92724f518682e6e965ccbac5
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 15:44:14 +0000

    Add if sentence is None condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     for x in sentence:
-        if sentence == "":
+        if sentence == "" or sentence is None:
             return (0, None)
         else:
             return (len(sentence), sentence[0])
