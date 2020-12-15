59ad6544988736138c2b1cde605251d5e390fbdc
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-15 19:49:08 +0000

    Add beginning of function for 13, and betty compliant comment

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: pointer to head of linked list
+* empty list is considered a palindrome
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+return (0);
+}
