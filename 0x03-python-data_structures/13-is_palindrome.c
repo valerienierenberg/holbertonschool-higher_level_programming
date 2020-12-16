dba7ac8d4c041e1019736b777aad99598221cd1e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-16 16:49:51 +0000

    Add 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,55 +12,30 @@
 
 int is_palindrome(listint_t **head)
 {
-listint_t **left = head;
-listint_t **right = head;
-/* stop recursion when right becomes NULL */
-if (right == NULL)
-return (1);
-
-/*
-* If sub-list is not palindrome then no need to
-* check for current left and right, return false
-*/
-bool isp = is_palindrome(head);
-if (isp == false)
-return (0);
-
-/* Check values at current left and right */
-bool isp1 = ((*right)->n == (*left)->n);
-
-/* Move left to next node */
-*left = (*left)->next;
-
-return (isp1);
+if (!*head)
+	return (1);
+	return (pal_help(head, *head));
 }
 
-/*
-* A wrapper over isPalindromeUtil()
-* bool isPalindrome(listint_t *head)
-* {
-* is_palindrome(**head);
-* return (0);
-* }
-*/
 
 /**
-* push - Push a node to linked list. Note that this function
-* changes the head
-* @head_ref: blah
-* @new_data: new data
+* pal_meat - helper function to check if list is palindrome
+* @left: start of list
+* @right: end of list
+* empty list is considered a palindrome
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
 */
-void push(listint_t **head_ref, char new_data)
+
+int pal_meat(listint_t **left, listint_t *right)
 {
-/* allocate node */
-listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
-
-/* put in the data  */
-new_node->n = new_data;
-
-/* link the old list off the new node */
-new_node->next = (*head_ref);
-
-/* move the head to pochar to the new node */
-(*head_ref) = new_node;
+if (!right)
+	return (1);
+if (pal_meat(left, right->next && (*left)->n == right->next))
+{
+	*left = (*left)->next;
+	return (1);
 }
+else
+	return (0);
+}
+
