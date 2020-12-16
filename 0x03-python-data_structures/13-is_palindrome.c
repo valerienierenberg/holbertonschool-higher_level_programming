f67a8d5781cd1b7e24113d712da4930a3ed90b1e
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-16 16:16:47 +0000

    Add 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h> 
-#include <stdbool.h> 
+#include <stdlib.h>
+#include <stdbool.h>
 
 /**
 * is_palindrome - checks if a singly linked list is a palindrome
@@ -12,50 +12,55 @@
 
 int is_palindrome(listint_t **head)
 {
-    listint_t **left = head;
-    listint_t **right = head;
-   /* stop recursion when right becomes NULL */
-   if (right == NULL) 
-      return true; 
-  
-   /* If sub-list is not palindrome then no need to 
-       check for current left and right, return false */
-   bool isp = is_palindrome(head); 
-   if (isp == false) 
-      return false; 
-  
-   /* Check values at current left and right */
-   bool isp1 = ((*right)->n == (*left)->n); 
-  
-   /* Move left to next node */
-   *left = (*left)->next; 
-  
-   return isp1; 
-} 
+listint_t **left = head;
+listint_t **right = head;
+/* stop recursion when right becomes NULL */
+if (right == NULL)
+return (1);
 
-/**
+/*
+* If sub-list is not palindrome then no need to
+* check for current left and right, return false
+*/
+bool isp = is_palindrome(head);
+if (isp == false)
+return (0);
+
+/* Check values at current left and right */
+bool isp1 = ((*right)->n == (*left)->n);
+
+/* Move left to next node */
+*left = (*left)->next;
+
+return (isp1);
+}
+
+/*
 * A wrapper over isPalindromeUtil()
 * bool isPalindrome(listint_t *head)
-* { 
-*   is_palindrome(**head);
-*   return (0);
+* {
+* is_palindrome(**head);
+* return (0);
 * }
 */
-  
-/* Push a node to linked list. Note that this function 
-  changes the head */
-void push(listint_t **head_ref, char new_data) 
-{ 
-    /* allocate node */
-    listint_t* new_node = (listint_t*) malloc(sizeof(listint_t)); 
-  
-    /* put in the data  */
-    new_node->n  = new_data; 
-  
-    /* link the old list off the new node */
-    new_node->next = (*head_ref);
-  
-    /* move the head to pochar to the new node */
-    (*head_ref)    = new_node; 
-} 
-  
+
+/**
+* push - Push a node to linked list. Note that this function
+* changes the head
+* @head_ref: blah
+* @new_data: new data
+*/
+void push(listint_t **head_ref, char new_data)
+{
+/* allocate node */
+listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+
+/* put in the data  */
+new_node->n = new_data;
+
+/* link the old list off the new node */
+new_node->next = (*head_ref);
+
+/* move the head to pochar to the new node */
+(*head_ref) = new_node;
+}
