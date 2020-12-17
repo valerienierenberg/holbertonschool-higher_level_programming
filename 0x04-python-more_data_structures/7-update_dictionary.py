7355b2bb29e7eb7f49b793cbc78915ee20c2d836
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-17 16:26:07 +0000

    Add comment

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,3 +5,6 @@ def update_dictionary(a_dictionary, key, value):
 
     a_dictionary[key] = value
     return a_dictionary
+
+# if the key doesn't exist, it will be created,
+# if the key does exist already, the value will be overwritten
