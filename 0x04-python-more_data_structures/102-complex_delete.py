648b162bcbc281c5c80935360d871814ee5e6c49
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-17 18:29:31 +0000

    Add 102-complex_delete.py, not complete. runs but does not delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+
+    if not a_dictionary:
+        return
+    for k, v in a_dictionary.items():
+        if v == 'value':
+            del a_dictionary[k]
+    return a_dictionary
