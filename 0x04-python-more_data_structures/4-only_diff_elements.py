8ede8c3360d1fe25d2a9013e52b46321de8f1068
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-17 16:02:41 +0000

    Add 4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+
+    result = []
+    for element in set_1:
+        if element not in set_2:
+            result.append(element)
+    for element in set_2:
+        if element not in set_1:
+            result.append(element)
+    return result
