cf7f35c64801b89816af41aba468ab3431637bfb
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-17 18:07:33 +0000

    Change wording

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary:
-        return (max(a_dictionary))
-    else:
-        return None
+    if a_dictionary is None:
+        return
+    return (max(a_dictionary))
