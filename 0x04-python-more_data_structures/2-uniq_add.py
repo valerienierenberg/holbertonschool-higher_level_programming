c34f524140431441f43ec043ad3ed3b3ba3e4fee
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-17 15:51:21 +0000

    Add 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+
+    return (sum(set(my_list)))
+
+#    newlist = my_list[:]
+#    total = 0
+
+#    for x in range(len(newlist) - 1):
+#        if (newlist[x] != newlist[x+1]):
+#            total = total + newlist[x+1]
+#    return total
+# (this gives result of 17.... couldn't quite get this version to work)
