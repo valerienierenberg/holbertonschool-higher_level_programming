c8ec0b7b966bb16b810b4a64703e5990e45346fc
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-18 15:25:17 +0000

    Add if not isinstance case for if not a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,9 @@ def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
 
+    if not isinstance(roman_string, str):
+        return 0
+
     if roman_string is None or not rom_val:
         return 0
 
