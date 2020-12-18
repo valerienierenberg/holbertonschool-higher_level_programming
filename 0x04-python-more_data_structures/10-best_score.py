ed2907ce29a645e520966b6177cdd0ed72d96609
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-18 17:01:49 +0000

    Remove lambda

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
 
     if not a_dictionary:
         return
-    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
-    return (max_key)
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
