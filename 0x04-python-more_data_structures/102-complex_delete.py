44f2bb30e6e04f69545bcd65faa338c11fd79aa8
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-18 16:45:25 +0000

    Change sorted to list

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@
 
 def complex_delete(a_dictionary, value):
     if a_dictionary:
-        for key in sorted(a_dictionary.keys()):
+        for key in list((a_dictionary.keys())):
             if a_dictionary[key] == value:
                 a_dictionary.pop(key)
     return (a_dictionary)
