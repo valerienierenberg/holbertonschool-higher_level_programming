84e9a08379ebbcf8c4c46db59b9cab7097ad3321
valerienierenberg <valerie.nierenberg@gmail.com>
2020-12-18 15:13:22 +0000

    Add None check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,9 @@ def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
 
+    if roman_string is None:
+        return
+
     for i in range(len(s)):
         if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
             int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
