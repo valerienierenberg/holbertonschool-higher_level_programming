852032e8c01adb48a8780cc5ba6401fd8065e95b
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-04 19:43:53 +0000

    Change range to just list length

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     my_list_3 = []
-    for y in range(0, list_length):
+    for y in range(list_length):
         try:
             my_list_3.append(my_list_1[y] / my_list_2[y])
         except ZeroDivisionError:
@@ -17,3 +17,6 @@ def list_division(my_list_1, my_list_2, list_length):
             pass
 
     return (my_list_3)
+
+# make an empty list called 'my_list_3'
+#
