79888769ebdb1f74fee14e5b7712db5691b2083d
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-04 16:17:38 +0000

    Add updated version with for loop outside of try and except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for x in my_list[:x]:
-            print("{}".format(my_list[x - 1]), end="")
-        print()
-    except IndexError:
-        print()
-    finally:
-        return x
+    a = 0
+    for y in range(x):
+        try:
+            print("{}".format(my_list[y]), end="")
+            a += 1
+        except IndexError:
+            break
+    print("")
+    return(a)
+
+
+# --gives correct output--
+# def safe_print_list(my_list=[], x=0):
+#    try:
+#        for x in my_list[:x]:
+#            print("{}".format(my_list[x - 1]), end="")
+#        print()
+#    except IndexError:
+#        print()
+#    finally:
+#        return x
