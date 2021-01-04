cfe89164a29771a0d8b57f3a4db69b1870008ebd
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-04 20:05:37 +0000

    Add comment

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from __future__ import print_function
 import sys
 
 
@@ -10,3 +9,10 @@ def safe_function(fct, *args):
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return None
+
+# Function that executes a function safely.
+# import sys module in order to print to stderr file
+# try: set result of function 'fct' to variable 'funcresult'
+#      return that result
+# except: if anything went wrong while running function 'fct'...
+# ...print error argument to stderr, and return None
