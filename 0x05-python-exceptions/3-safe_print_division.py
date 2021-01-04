5f1be316a56a5a91231fa908208506fd60f7084f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-04 19:39:24 +0000

    Add comment

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,3 +9,10 @@ def safe_print_division(a, b):
         return None
     finally:
         print("Inside result: {}".format(x))
+
+# Function that divides 2 integers and prints the result.
+# initialize result (x) to 0
+# try: set x to the result of 'a' divided by 'b'
+# (return result of division)
+# except: if there is a zerodivisionerror, set x to None and return None
+# finally: print the result of division
