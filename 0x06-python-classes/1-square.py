c230695efa64681258207f494b9de319968f8525
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-05 22:00:53 +0000

    Add module docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!usr/bin/python3
+""" This module contains an empty class to practice class
+declarations and definitons """
+
 class Square:
     """ Square class """
     def __init__(self, size):
