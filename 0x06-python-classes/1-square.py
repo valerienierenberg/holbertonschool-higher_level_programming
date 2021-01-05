d362da26241a893e07f77c2fe946bbf540342d14
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-05 22:03:31 +0000

    Add docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,19 @@
 """ This module contains an empty class to practice class
 declarations and definitons """
 
+
 class Square:
-    """ Square class """
+    """ Square class - represents a square
+
+    Attributes:
+        __size : integer, size of a side of the square
+    """
     def __init__(self, size):
+        """ Initializes a square
+
+        Args:
+            size: integer, size of side of square
+
+        Returns: None
+        """
         self.__size = size
