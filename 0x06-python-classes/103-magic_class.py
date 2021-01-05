e158b8ef4f58b92101356b7a615a61b4987c2590
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-05 22:48:00 +0000

    Add 103-magic_class.py

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains a class MagicClass """
+
+
+class MagicClass:
+    """ Square class """
+    def __init__(self, radius):
+        """ __init method
+        Args:
+            size: integer
+            position: tuple of integers
+        Returns:
+            None
+        """
+        self.__radius = __radius
