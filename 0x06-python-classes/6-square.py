d3358dfeaf8e05a8a5b5ba9df846e2053aa577a0
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-06 15:40:56 +0000

    Add None conditions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
         """
         if len(value) == 1 or type(value) != tuple or type(value[0]) is not \
                 int or type(value[1]) is not int or value[0] < 0 \
-                or value[1] < 0:
+                or value[1] < 0 or value[0] is None or value[1] is None:
             raise TypeError('position must be a tuple of 2 positive integers')
         if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
