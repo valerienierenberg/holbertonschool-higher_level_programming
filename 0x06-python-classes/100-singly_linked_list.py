22755ca217bb371b24c10f6729d751b1ca9fa2ef
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-06 20:44:49 +0000

    Add updates, still not finished

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -29,7 +29,7 @@ class Node:
             TypeError: if size is not an int
         """
         if type(value) is not int:
-            raise TypeError('size must be an integer')
+            raise TypeError('data must be an integer')
         else:
             self.__data = value
 
@@ -49,7 +49,7 @@ class Node:
         Raises:
             TypeError: if next_node is not None or a Node
         """
-        if type(value) is not Node and not None:
+        if type(value) is not Node and value is not None:
             raise TypeError('next_node must be a Node object')
         else:
             self.__next_node = value
@@ -65,6 +65,21 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
-        value = value
+        """ sorted_insert method
+        Args:
+            self, value - int
+        """
+        newNode = Node(value)
+        if self.head is None:
+            self.__head = Node(value)
+        if self.__head.data < value:
+            newNode.next_node = self.__head
+            self.__head = newNode
 
-# UNFINISHED....
+        # check if new_node is less than head
+        # check if head is none
+        # check if next_node is larger than new node
+        # check if we're at end of node
+        # create new node with value
+        # change existing next node to point to new node and new node to
+        # point to next node if exists
