fc4834f4cb814956e7a18753fa8f5f032f4bea9f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-19 20:25:36 +0000

    Add 100-my_int.py - functioning opposite of how it should at this point

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contains a class MyInt that inherits from int.
+    However, MyInt has == and != operators inverted """
+
+
+class MyInt(int):
+    """ MyInt class """
+    def new():
+        if (MyInt.__and):
+            return False
+        if (MyInt.__ne):
+            return True
