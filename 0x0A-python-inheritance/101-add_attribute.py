9acb120353c7d28fb47189532bf5d1ee0901f332
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-19 20:39:28 +0000

    Add 101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class MyClass():
+    pass
+
+def add_attribute(obj, name, value):
+    """ is_same_class method:
+        args: obj - object to create new attribute for
+              name - name of attribute
+              value - value (string)
+        raises: TypeError: if the object can't have new attribute
+        return:
+            None
+    """
+    if obj is None or name is None or value is None:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
