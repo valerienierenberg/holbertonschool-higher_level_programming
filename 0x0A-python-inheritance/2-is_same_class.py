6ddafa8b7808fb63021a37e28b84fa4e28a6907e
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-19 17:28:00 +0000

    Add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module contains a class MyList that inherits from list """
+
+
+def is_same_class(obj, a_class):
+    """ is_same_class method:
+        args: obj - object to be tested
+              a_class - class that object is being tested against
+        return:
+            true if object is exactly an instance of a_class
+            otherwise false
+    """
+    return (type(obj) == a_class)
+
+#  another way to do it:
+#  if a_class is not object:
+#     return isinstance(obj, a_class)
