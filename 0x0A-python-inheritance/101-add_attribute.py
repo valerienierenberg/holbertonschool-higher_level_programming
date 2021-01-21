834f00409fc7346515bb1a521ed2df131c961595
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-21 17:08:21 +0000

    Add __dict__ condition

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-
+""" This module contains a function that
+    adds a new attribute to an object if it’s possible """
 
 
 class MyClass():
     pass
 
-def add_attribute(obj, name, value):
-    """ is_same_class method:
-        args: obj - object to create new attribute for
-              name - name of attribute
-              value - value (string)
-        raises: TypeError: if the object can't have new attribute
-        return:
-            None
-    """
-    if obj is None or name is None or value is None:
-        raise TypeError("can't add new attribute")
-    else:
-        setattr(obj, name, value)
+    def add_attribute(obj, name, value):
+        """ is_same_class method:
+            args: obj - object to create new attribute for
+                name - name of attribute
+                value - value (string)
+            raises: TypeError: if the object can't have new attribute
+            return:
+                None
+        """
+        if '__dict__' not in dir(obj):
+            raise TypeError("can't add new attribute")
+        else:
+            setattr(obj, name, value)
