bc275091ae0f351bf4bc4df6cb9aeb00550c65b1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-03 22:49:28 +0000

    Add 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,9 @@
---script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_genres.name AS genre;
-FROM tv_show_genres;
-ORDER BY tv_show_genres.tv_genres ASC;
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
