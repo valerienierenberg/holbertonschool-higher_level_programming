3b72eb61e58b42e230e62bde173b58ca6e6ac96c
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-04 00:45:32 +0000

    Add 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,9 @@
---script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_genres.name AS genre;
-FROM tv_show_genres;
-ORDER BY tv_show_genres.tv_genres ASC;
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+    (SELECT tv_genres.name FROM tv_genres
+    JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
