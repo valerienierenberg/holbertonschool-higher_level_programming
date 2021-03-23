2870ebb3ac40bf85414201b3bee320e07cd19feb
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-23 16:21:40 +0000

    Add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let i = 0;
+
+    if (!c) {
+      c = 'X';
+    }
+    for (i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
