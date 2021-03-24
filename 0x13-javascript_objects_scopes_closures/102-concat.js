df6e1326afc91a1ebd425c0ff5aa839464e63459
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-24 15:58:58 +0000

    semistandard --fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -7,11 +7,11 @@ const fileB = process.argv[3];
 const fileC = process.argv[4];
 
 fs.readFile(fileA, function (err, dataA) {
+  if (err) throw err;
+  fs.readFile(fileB, function (err, dataB) {
     if (err) throw err;
-    fs.readFile(fileB, function (err, dataB) {
-        if (err) throw err;
-        fs.appendFile(fileC, dataA + dataB, function (err) {
-            if (err) throw err;
-        });
+    fs.appendFile(fileC, dataA + dataB, function (err) {
+      if (err) throw err;
     });
+  });
 });
