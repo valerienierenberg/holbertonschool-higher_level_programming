fd03cf1f937475880f9391d90fc6673ce7a88762
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-24 15:57:35 +0000

    Add task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+fs.readFile(fileA, function (err, dataA) {
+    if (err) throw err;
+    fs.readFile(fileB, function (err, dataB) {
+        if (err) throw err;
+        fs.appendFile(fileC, dataA + dataB, function (err) {
+            if (err) throw err;
+        });
+    });
+});
