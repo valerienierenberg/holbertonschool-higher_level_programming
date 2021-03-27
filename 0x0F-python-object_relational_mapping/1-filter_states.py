f5af4168ab62d5d527f225128281ea1fc5460c98
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-27 15:37:03 +0000

    remove charset specification

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
     conn = MySQLdb.connect(user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * from states WHERE name like 'N%' ORDER BY
-                states.id """)
+    cur.execute("""SELECT * from states WHERE name like 'N%'
+                ORDER BY states.id """)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
