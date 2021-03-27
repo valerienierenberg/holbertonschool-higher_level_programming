53e39d483683f8b94a809fefffc3782759057159
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-27 15:40:44 +0000

    Add collate

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name='{}' ORDER BY
-               id ASC""".format(argv[4]))
+    cur.execute("""SELECT * FROM states WHERE name='{}'
+                COLLATE latin1_general_cs ORDER BY
+                states.id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
