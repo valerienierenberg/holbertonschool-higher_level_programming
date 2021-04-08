95813be78c8d5da84e2038c68282802fc53efb42
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-08 17:04:47 +0000

    Remove -i flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get request that displays the body of the response
-curl -sX GET "$1" -i
+curl -sX GET "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept
-curl -sX OPTIONS "$1" -i
+curl -sX OPTIONS "$1"
