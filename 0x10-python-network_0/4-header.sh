c8f13c6da839a1fb8b815beec4bbfbcfa0f07088
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-08 17:12:03 +0000

    Make comment one line

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays
-# the body of the response. A header variable X-HolbertonSchool-User-Id must be
-# sent with the value 98
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response. A header variable X-HolbertonSchool-User-Id must be sent with the value 98
 curl -sX GET "$1" -H X-HolbertonSchool-User-Id=98
