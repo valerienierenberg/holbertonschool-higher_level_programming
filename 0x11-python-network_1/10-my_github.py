721e8a82e143c64a8bfc3d6f8255b842829b0e34
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-09 21:00:49 +0000

    change single quotes to double

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,6 @@ and uses the GitHub API to display your id
 """
 import requests
 from sys import argv
-from pprint import pprint
 
 
 if __name__ == '__main__':
@@ -12,9 +11,9 @@ if __name__ == '__main__':
     username = argv[1]
     password = argv[2]
 
-    r = requests.get('https://api.github.com/user', auth=(username,
+    r = requests.get("https://api.github.com/user", auth=(username,
                                                           password)).json()
     if 'id' in r:
         print(r['id'])
     else:
-        print('None')
+        print(None)
