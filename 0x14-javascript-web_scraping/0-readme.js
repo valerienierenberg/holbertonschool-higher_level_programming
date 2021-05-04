568fb06e25177eb27067d23ba3ea5a77737a8be3
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-04 16:08:52 +0000

    Correct error object output

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,9 @@ const process = require('process');
 const myargs = process.argv.slice(2);
 
 fs.readFile(myargs[0], (err, data) => {
-  if (err) throw err;
-
-  console.log(data.toString());
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
