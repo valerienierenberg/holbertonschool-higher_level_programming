10cbdbd9ea592864a678cdd797dde8cd5ad37c92
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-05 23:15:58 +0000

    remove length

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(host, function (err, res, body) {
     return;
   }
   let occ = 0;
-  for (const film of JSON.parse(body).results.length) {
+  for (const film of JSON.parse(body).results) {
     for (const character of film.characters) {
       if (parseInt(character.slice(37, 39)) === 18) {
         occ = occ + 1;
