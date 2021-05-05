b9b82569c34d2db912a47eeb118776fbac4adeda
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-05 23:20:50 +0000

    .includes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,11 @@ const host = process.argv[2];
 request(host, function (err, res, body) {
   if (err) {
     console.error(err);
-    return;
   }
   let occ = 0;
   for (const film of JSON.parse(body).results) {
     for (const character of film.characters) {
-      if (parseInt(character.slice(37, 39)) === 18) {
+      if (parseInt(character.includes('18'))) {
         occ = occ + 1;
       }
     }
