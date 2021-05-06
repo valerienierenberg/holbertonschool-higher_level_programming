2f2a690ae4a8a3b536a7b58ac0e6feb7fddd787b
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-06 18:51:38 +0000

    semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,15 +7,16 @@ request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', func
   if (err) {
     console.log(err);
   } else {
-      let whole = JSON.parse(body);
-      for (let char of whole.characters) {
-          request.get(char, function (err, res, body) {
-              let wholeChars = JSON.parse(body);
-              if (err) {
-                  console.log(err);
-              } else {
-                  console.log(wholeChars.name); }
-              });
-            }
+    const whole = JSON.parse(body);
+    for (const char of whole.characters) {
+      request.get(char, function (err, res, body) {
+        const wholeChars = JSON.parse(body);
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(wholeChars.name);
         }
-    });
+      });
+    }
+  }
+});
