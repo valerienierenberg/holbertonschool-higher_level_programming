fe5dfd3e3ab327279ededad1b360f53c8fe86f55
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-12 17:37:33 +0000

    Add task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,5 @@
+// fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+// using JQuery API
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('#character').append(data.name);
+});
