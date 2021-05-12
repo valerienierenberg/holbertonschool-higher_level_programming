212e159fc2b96dfc40dfe859c672d3a30cdfaef1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-12 16:41:38 +0000

    Add task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+// toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header
+// using JQuery API
+$('#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});
