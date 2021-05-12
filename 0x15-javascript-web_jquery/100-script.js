307c93552210ba2cae23bbd6f6882ab5b341d5d7
valerienierenberg <valerie.nierenberg@gmail.com>
2021-05-12 18:07:40 +0000

    Add task 100 - addEventListener

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// script to update color of html header tag to red
+// using document.querySelector (script must be imported from the <head> tag, not at the end of the HTML)
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style = 'color: red;';
+});
